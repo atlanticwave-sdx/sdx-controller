23a1f0282a12a8b77c73819dc362c0ea336649bc
Cong Wang <cwang@rciadmins-MBP.attlocal.net>
2021-06-23 21:33:13 -0400

    integrate mq with swagger

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -3,9 +3,18 @@
 import connexion
 
 from swagger_server import encoder
+from swagger_server.messaging.message_queue import *
 
 
 def main():
+    # Start listening RabbitMQ
+    serverconfigure = RabbitMqServerConfigure(host='localhost',
+                                            queue='hello')
+
+    server = rabbitmqServer(server=serverconfigure)
+    server.startserver()
+
+    # Run swagger service
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'SDX-Controller'}, pythonic_params=True)
--- swagger_server/controllers/topology_controller.py
+++ swagger_server/controllers/topology_controller.py
@@ -4,7 +4,6 @@ import six
 from swagger_server.models.topology import Topology  # noqa: E501
 from swagger_server import util
 
-
 def get_topology():  # noqa: E501
     """get an existing topology
 
