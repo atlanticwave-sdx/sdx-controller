d696169e44defcb37baa69a043512280d4774ad3
Cong Wang <cwang@rciadmins-MBP.attlocal.net>
2021-06-30 22:22:35 -0400

    add mq rpc

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -3,16 +3,23 @@
 import connexion
 
 from swagger_server import encoder
-from swagger_server.messaging.message_queue import *
-
+from swagger_server.messaging.message_queue_consumer import *
+from swagger_server.messaging.rpc_queue_consumer import *
 
 def main():
     # Start listening RabbitMQ
-    serverconfigure = RabbitMqServerConfigure(host='localhost',
-                                            queue='hello')
+    # serverconfigure = RabbitMqServerConfigure(host='localhost',
+    #                                         queue='hello')
 
-    server = rabbitmqServer(server=serverconfigure)
-    server.startserver()
+    # server = rabbitmqServer(server=serverconfigure)
+    # server.startserver()
+
+    # message queue rpc test
+    rpc = RpcClient()
+    body = "test body"
+    print("Published Message: {}".format(body))
+    response = rpc.call(body)
+    print(" [.] Got response: " + str(response))
 
     # Run swagger service
     app = connexion.App(__name__, specification_dir='./swagger/')
