21eec24693b9955cf27e0404644041b062a8a746
Yufeng Xin <yxin@renci.org>
2022-05-31 02:37:26 -0400

    unittest for path breakdown #9 #3

--- tests/test_Solver.py
+++ tests/test_Solver.py
@@ -6,6 +6,7 @@ import networkx as nx
 from sdxdatamodel.parsing import *
 
 from sdxdatamodel.topologymanager.temanager import TEManager
+from sdxdatamodel.parsing.exceptions import DataModelException
 
 from LoadBalancing.MC_Solver import runMC_Solver
 from LoadBalancing.RandomTopologyGenerator import GetConnection
@@ -18,6 +19,12 @@ from LoadBalancing.RandomTopologyGenerator import lbnxgraphgenerator
 
 TOPOLOGY = "./tests/data/sdx.json"
 CONNECTION = "./tests/data/test_request.json"
+
+TOPOLOGY_AMLIGHT = './tests/data/amlight.json'
+TOPOLOGY_SAX = './tests/data/sax.json'
+TOPOLOGY_ZAOXI = './tests/data/zaoxi.json'
+
+topology_file_list_3 = [TOPOLOGY_AMLIGHT,TOPOLOGY_SAX, TOPOLOGY_ZAOXI]
 
 class Test_Solver(unittest.TestCase):
 
@@ -33,7 +40,7 @@ class Test_Solver(unittest.TestCase):
         with open('./tests/data/connection.json', 'w') as json_file:
             json.dump(self.connection, json_file, indent=4)
 
-    def test_Computation(self):
+    def test_computation(self):
         num_nodes = self.graph.number_of_nodes()
         print("num of nodes:"+str(num_nodes))
         print(self.graph.edges)
@@ -43,6 +50,28 @@ class Test_Solver(unittest.TestCase):
         print(result)
         #self.assertEqual(self.solution, result)
 
+    def test_computation_breakdown(self):
+        try:
+            for topology_file in topology_file_list_3:
+                with open(topology_file, 'r', encoding='utf-8') as data_file:
+                    data = json.load(data_file)
+                print("Adding Topology:" + topology_file)
+                self.temanager.manager.add_topology(data) 
+        except DataModelException as e:
+            print(e)
+            return False 
+        self.graph =  self.temanager.generate_graph_te()
+        self.connection=self.temanager.generate_connection_te()
+        with open('./tests/data/connection.json', 'w') as json_file:
+            json.dump(self.connection, json_file, indent=4)
+        num_nodes = self.graph.number_of_nodes()
+        lbnxgraphgenerator(num_nodes, 0.4, self.connection, self.graph)
+        result = runMC_Solver()
+
+        result = runMC_Solver()
+        print(result)
+        breakdown = self.temanager.generate_connection_breakdown(result)
+        print(breakdown)
 
 if __name__ == '__main__':
     unittest.main()
