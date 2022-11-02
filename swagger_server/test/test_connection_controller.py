8c365b378f38a22be20d278ef0e31b9a8d325e50
Sajith Sasidharan <sajith@hcoop.net>
2022-11-02 16:35:16 -0500

    Do setup before placing a test connection

--- swagger_server/test/test_connection_controller.py
+++ swagger_server/test/test_connection_controller.py
@@ -3,14 +3,15 @@
 from __future__ import absolute_import
 
 import datetime
+import os
 
 from flask import json
-
 from sdxdatamodel.models.connection import Connection
 # from sdxdatamodel.models.location import Location
 # from sdxdatamodel.models.node import Node
 from sdxdatamodel.models.port import Port
 from swagger_server.test import BaseTestCase
+from swagger_server.utils.db_utils import DbUtils
 
 
 class TestConnectionControllerBasic(BaseTestCase):
@@ -55,10 +56,100 @@ class TestPlaceConnectionFailures(BaseTestCase):
     """
 
     def setUp(self):
+        # Default DB name is just "test", when the env var is not set.
+        db_name = os.environ.get("DB_NAME", "test")
+        db_name = f"{db_name}.sqlite3"
+
+        db_tuples = [("config_table", "test-config")]
+        
+        db = DbUtils()
+        db._initialize_db(db_name, db_tuples)
+
+        # TODO: ideally db shouldn't have any topology at this point;
+        # but it possibly can.
+        topo_val = db.read_from_db("latest_topo")
+        if topo_val is None:
+            self.add_topology_to_db(db)
+            
+    def tearDown(self):
+        # TODO: Clear DB.
         pass
 
-    def tearDown(self):
-        pass
+    def add_topology_to_db(self, db):
+        """Add a topology value to DB."""
+        ingress_port = {
+            "id": "ingress_port_id",
+            "name": "ingress_node_port_name",
+            "short_name": "ingress_node_port_short_name",
+            "node": "ingress_node_name",
+            "state": "unknown",
+            "status": "unknown",
+        }
+
+        ingress_node = {
+            "id": "ingress_node_id",
+            "name": "ingress_node_name",
+            "short_name": "ingress_node_short_name",
+            "ports": [ ingress_port ],
+            "location": {
+                "address": "unknown",
+                "latitude": "unknown",
+                "longitude": "unknown",
+            },
+        }
+
+        egress_port = {
+            "id": "egress_port_id",
+            "name": "egress_node_port_name",
+            "short_name": "egress_node_port_short_name",
+            "node": "egress_node_name",
+            "state": "unknown",
+            "status": "unknown",
+        }
+
+        egress_node = {
+            "id": "egress_node_id",
+            "name": "egress_node_name",
+            "short_name": "egress_node_short_name",
+            "ports": [ egress_port ],
+            "location": {
+                "address": "unknown",
+                "latitude": "unknown",
+                "longitude": "unknown",
+            },
+        }
+
+        links = [
+            {
+                "id": "ingress_link_id",
+                "name": "ingress_link_name",
+                "short_name": "ingress_link_short_name",
+                "residual_bandwidth": 1000.0,
+                "ports": [ ingress_port, ingress_port ],
+            },
+            {
+                "id": "egress_link_id",
+                "name": "egress_link_name",
+                "short_name": "egress_link_short_name",
+                "residual_bandwidth": 1000.0,
+                "ports": [ egress_port, egress_port ],
+            },
+        ]
+
+        topology = {
+            "id": "test_topo_id",
+            "name": "test_topo_name",
+            "version": "test_topo_version",
+            "time_stamp": None,
+            "nodes": [ingress_node, egress_node],
+            "links": links,
+        }        
+        
+        topo_val = json.dumps(topology)
+        print("Made a workaround topo_val: {}".format(topo_val))
+
+        db.add_key_value_pair_to_db("latest_topo", topo_val)
+
 
     def test_place_connection(self):
         """Test case for place_connection
