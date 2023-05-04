7c591bd9cb25bf9777095e8813630ba56d6154de
Cong Wang <cwang@renci.org>
2023-05-04 11:06:40 -0400

    Adapt connection handler with MongoDB

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -137,10 +137,10 @@ def start_consumer(thread_queue, db_instance):
                 else:
                     logger.info("got message from MQ: " + str(msg))
             else:
-                db_instance.add_key_value_pair_to_db(MESSAGE_ID, msg)
+                db_instance.add_key_value_pair_to_db(str(MESSAGE_ID), msg)
                 logger.debug("Save to database complete.")
                 logger.debug("message ID:" + str(MESSAGE_ID))
-                value = db_instance.read_from_db(MESSAGE_ID)
+                value = db_instance.read_from_db(str(MESSAGE_ID))
                 logger.debug("got value from DB:")
                 logger.debug(value)
                 MESSAGE_ID += 1
