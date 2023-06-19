4c40d0f376131711eda848d097e8c95bff68c4b5
Ubuntu <ubuntu@ip-172-31-3-123.ec2.internal>
2023-06-19 19:44:31 +0000

    worker process

---
+++ worker/app.py
@@ -0,0 +1,36 @@
+""" rabit mq worker process """
+import time
+import pika
+
+SLEEP_TIME = 20
+print(' [*] Sleeping for ', SLEEP_TIME, ' seconds.')
+time.sleep(SLEEP_TIME)
+
+print(' [*] Connecting to server ...')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+channel = connection.channel()
+channel.queue_declare(queue='task_queue', durable=True)
+
+print(' [*] Waiting for messages.')
+
+
+def callback(ch, method, properties, body):
+    """ call back """
+    print(f" [x] Received {body}")
+    cmd = body.decode()
+
+    if cmd == 'hey':
+        print("hey there")
+    elif cmd == 'hello':
+        print("well hello there")
+    else:
+        print("sorry i did not understand ", body)
+
+    print(" [x] Done")
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+channel.start_consuming()
