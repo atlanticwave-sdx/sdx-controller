6d38b4682f5d83e7aa37cb98a29a85f0d32b8085
Ubuntu <ubuntu@ip-172-31-3-123.ec2.internal>
2023-06-22 17:58:19 +0000

    adding mq worker

--- worker/app.py
+++ worker/app.py
@@ -2,19 +2,20 @@
 import time
 import pika
 
-SLEEP_TIME = 20
+SLEEP_TIME = 5
 print(' [*] Sleeping for ', SLEEP_TIME, ' seconds.')
 time.sleep(SLEEP_TIME)
 
 print(' [*] Connecting to server ...')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+credentials = pika.PlainCredentials('mq_user', 'mq_pwd')
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq3', 5672, '/', credentials))
 channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 
 print(' [*] Waiting for messages.')
 
 
-def callback(ch, method, properties, body):
+def callback(call_channel, method, properties, body):
     """ call back """
     print(f" [x] Received {body}")
     cmd = body.decode()
@@ -28,7 +29,7 @@ def callback(ch, method, properties, body):
 
     print(" [x] Done")
 
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    call_channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
 channel.basic_qos(prefetch_count=1)
