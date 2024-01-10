484830fbc0550872cee6629ff646ef2909758469
Sajith Sasidharan <sajith@hcoop.net>
2024-01-10 17:00:29 -0600

    Use _topology_map, not topology_list

--- swagger_server/handlers/connection_handler.py
+++ swagger_server/handlers/connection_handler.py
@@ -120,7 +120,9 @@ class ConnectionHandler:
             )
             temanager.add_topology(curr_topo_json)
 
-        for num, val in enumerate(temanager.topology_manager.topology_list):
+        # TODO: using a private variable here -- pce should have a
+        # getter.
+        for num, val in enumerate(temanager.topology_manager._topology_map.values()):
             logger.info(f"TE topology #{num}: {val}")
 
         graph = temanager.generate_graph_te()
