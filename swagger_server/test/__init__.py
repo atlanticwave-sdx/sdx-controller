ab467cc4212bd16597e1c66f6f0ebbe6a757069e
Sajith Sasidharan <sajith@hcoop.net>
2024-01-23 18:29:31 -0600

    Use the common create_app()

--- swagger_server/test/__init__.py
+++ swagger_server/test/__init__.py
@@ -4,12 +4,13 @@ import connexion
 from flask_testing import TestCase
 
 from swagger_server.encoder import JSONEncoder
+from swagger_server.__main__ import create_app
 
 
 class BaseTestCase(TestCase):
     def create_app(self):
         logging.getLogger("connexion.operation").setLevel("ERROR")
-        app = connexion.App(__name__, specification_dir="../swagger/")
-        app.app.json_encoder = JSONEncoder
-        app.add_api("swagger.yaml")
+
+        app = create_app()
+
         return app.app
