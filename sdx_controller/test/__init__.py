a56899f51217e01ac654d50f8da0135a059de993
Sajith Sasidharan <sajith@hcoop.net>
2024-01-29 21:48:27 -0600

    Use create_app() from top-level in BaseTestCase

--- sdx_controller/test/__init__.py
+++ sdx_controller/test/__init__.py
@@ -3,13 +3,11 @@ import logging
 import connexion
 from flask_testing import TestCase
 
-from sdx_controller.encoder import JSONEncoder
+from sdx_controller import create_app
 
 
 class BaseTestCase(TestCase):
+
     def create_app(self):
-        logging.getLogger("connexion.operation").setLevel("ERROR")
-        app = connexion.App(__name__, specification_dir="../swagger/")
-        app.app.json_encoder = JSONEncoder
-        app.add_api("swagger.yaml")
+        app = create_app(run_listener=False)
         return app.app
