482123bbcb87f3dca637c8c2ab7e04d36b5891f9
Sajith Sasidharan <sajith@hcoop.net>
2024-01-29 22:03:39 -0600

    Add a docstring

--- sdx_controller/app.py
+++ sdx_controller/app.py
@@ -9,6 +9,12 @@ app = application.app
 
 @atexit.register
 def on_app_exit():
+    """
+    Do some cleanup on exit.
+
+    We run a message queue consumer in a separate thread, and here we
+    signal the thread that we're exiting.
+    """
     logging.info("Stopping RPC threads")
     application.rpc_consumer.stop_threads()
 
