ef8c70c847524cd91d0fa1da7924794e1f0d451b
Sajith Sasidharan <sajith@hcoop.net>
2024-01-30 11:54:09 -0600

    Do not use message queue in testing when MQ_HOST is not set

--- sdx_controller/test/__init__.py
+++ sdx_controller/test/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import connexion
 from flask_testing import TestCase
@@ -9,5 +10,9 @@ from sdx_controller import create_app
 class BaseTestCase(TestCase):
 
     def create_app(self):
-        app = create_app(run_listener=False)
+        # Do not use the message queue if MQ_HOST is not set.  This is
+        # a useful work-around when we do not want to spin up a
+        # RabbitMQ insteance just for testing, since the test suite
+        # doesn't use a message queue right now.
+        app = create_app(run_listener=True if os.getenv("MQ_HOST") else False)
         return app.app
