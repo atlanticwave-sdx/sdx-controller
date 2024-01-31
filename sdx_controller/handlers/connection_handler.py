22825eb7ce56ad66878becb7e6e3d97e185fee7a
Sajith Sasidharan <sajith@hcoop.net>
2024-01-31 16:33:02 -0600

    Return success status and code from place_connection()

--- sdx_controller/handlers/connection_handler.py
+++ sdx_controller/handlers/connection_handler.py
@@ -160,6 +160,8 @@ class ConnectionHandler:
         breakdown = temanager.generate_connection_breakdown(solution)
         self._send_breakdown_to_lc(breakdown, connection_request)
 
+        return "OK", 200
+
     def handle_link_failure(self, msg_json):
         logger.debug("---Handling connections that contain failed link.---")
         link_connections_dict_str = self.db_instance.read_from_db(
