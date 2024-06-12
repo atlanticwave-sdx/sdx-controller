9943454edb511f4b8e2e3e98b930bec0749f1428
Sajith Sasidharan <sajith@hcoop.net>
2024-06-12 10:12:37 -0500

    Do less in on_request()

--- sdx_controller/messaging/rpc_queue_consumer.py
+++ sdx_controller/messaging/rpc_queue_consumer.py
@@ -44,24 +44,24 @@ class RpcConsumer(object):
         self._exit_event = threading.Event()
 
     def on_request(self, ch, method, props, message_body):
-        response = message_body
+        # response = message_body
         self._thread_queue.put(message_body)
 
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=MQ_HOST,
-                port=MQ_PORT,
-                credentials=pika.PlainCredentials(username=MQ_USER, password=MQ_PASS),
-            )
-        )
-        self.channel = self.connection.channel()
+        # self.connection = pika.BlockingConnection(
+        #     pika.ConnectionParameters(
+        #         host=MQ_HOST,
+        #         port=MQ_PORT,
+        #         credentials=pika.PlainCredentials(username=MQ_USER, password=MQ_PASS),
+        #     )
+        # )
+        # self.channel = self.connection.channel()
 
-        ch.basic_publish(
-            exchange=self.exchange_name,
-            routing_key=props.reply_to,
-            properties=pika.BasicProperties(correlation_id=props.correlation_id),
-            body=str(response),
-        )
+        # ch.basic_publish(
+        #     exchange=self.exchange_name,
+        #     routing_key=props.reply_to,
+        #     properties=pika.BasicProperties(correlation_id=props.correlation_id),
+        #     body=str(response),
+        # )
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
     def start_consumer(self):
