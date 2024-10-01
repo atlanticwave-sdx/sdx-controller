2755366b1f38ce9db9b23cddadc38228eb21a01e
Cong Wang <cwang@renci.org>
2024-10-01 08:31:09 -0400

    Add topology when initializing TE manager

--- sdx_controller/__init__.py
+++ sdx_controller/__init__.py
@@ -64,8 +64,15 @@ def create_app(run_listener: bool = True):
     app.db_instance = DbUtils()
     app.db_instance.initialize_db()
 
+    topo_val = app.db_instance.read_from_db("topologies", "latest_topo")
+    print("-----Topo_val-----")
+    print(topo_val["latest_topo"])
     # Get a handle to PCE.
-    app.te_manager = TEManager(topology_data=None)
+    app.te_manager = (
+        TEManager(topology_data=topo_val["latest_topo"])
+        if topo_val
+        else TEManager(topology_data=None)
+    )
 
     # TODO: This is a hack, until we find a better way to get a handle
     # to TEManager from Flask current_app, which are typically
