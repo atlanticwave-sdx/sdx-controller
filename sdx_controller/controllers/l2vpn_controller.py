f526486340dd5e34961448dd32910658405522ea
Cong Wang <cwang@renci.org>
2024-10-25 10:02:12 -0400

    Update unit tests

--- sdx_controller/controllers/l2vpn_controller.py
+++ sdx_controller/controllers/l2vpn_controller.py
@@ -140,7 +140,7 @@ def place_connection(body):
 
     reason, code = connection_handler.place_connection(current_app.te_manager, body)
 
-    if code == 200:
+    if code // 100 == 2:
         db_instance.add_key_value_pair_to_db(
             "connections", service_id, json.dumps(body)
         )
@@ -151,7 +151,7 @@ def place_connection(body):
 
     response = {
         "service_id": service_id,
-        "status": "OK" if code == 201 else "Failure",
+        "status": "OK" if code // 100 == 2 else "Failure",
         "reason": reason,
     }
 
@@ -163,7 +163,7 @@ def place_connection(body):
     # response = body
 
     # response["id"] = service_id
-    # response["status"] = "success" if code == 200 else "failure"
+    # response["status"] = "success" if code == 2xx else "failure"
     # response["reason"] = reason # `reason` is not present in schema though.
 
     return response, code
