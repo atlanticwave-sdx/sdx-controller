c4bc15cf9fde95acb1bd9a354f54986562168b48
Yufeng Xin <yxin@renci.org>
2025-02-23 11:12:05 -0500

    remove enum for constants: moved to datamodel already

--- sdx_controller/utils/sdx_utils.py
+++ sdx_controller/utils/sdx_utils.py
@@ -1,19 +1,3 @@
-class QueueExchange(Enum):
-    CONNECTION = auto()
-    OXP_UPDATE = auto()
-
-    def __str__(self):
-        return self.name
-
-
-class QueueTopic(Enum):
-    CONNECTION = auto()
-    TOPO = auto()
-
-    def __str__(self):
-        return self.name
-
-
 class TopologyMessage(object):
     def __init__(self, message, domain_name):
         self.message = message
