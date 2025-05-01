2aa469da000f4aa4c9d9a7962fdb7458378b8c6a
Cong Wang <cwang@renci.org>
2025-05-01 10:57:53 -0400

    add modifying transition

--- sdx_controller/controllers/l2vpn_controller.py
+++ sdx_controller/controllers/l2vpn_controller.py
@@ -71,6 +71,9 @@ def delete_connection(service_id):
             connection["status"] = str(ConnectionStateMachine.State.DELETED)
         else:
             connection, _ = connection_state_machine(
+                connection, ConnectionStateMachine.State.MODIFYING
+            )
+            connection, _ = connection_state_machine(
                 connection, ConnectionStateMachine.State.DOWN
             )
             connection, _ = connection_state_machine(
